4db3b93560e96f65ee25808aad7b1c1d79eccc59
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-05-12 22:23:57 -0300

    feat: add tests

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -2,7 +2,15 @@ defmodule ListFilterTest do
   use ExUnit.Case
   doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  test "return num of odd in list" do
+    list = ["1", "3", "6", "43", "banana", "6", "abc"]
+
+    assert ListFilter.call(list) == 3
+  end
+
+  test "return 0 if list is empty" do
+    list = []
+
+    assert ListFilter.call(list) == 0
   end
 end
